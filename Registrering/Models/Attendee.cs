a647a8e90fa2ce39c8a6cd8f22750db17d77d60f
John Inge <42311775+varleg@users.noreply.github.com>
2020-01-13 19:05:29 +0100

    Add project files.

---
+++ Registrering/Models/Attendee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registrering.Models
+{
+    public class Attendee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Accomodation { get; set; }
+
+        public DateTime SpecialDate { get; set; }
+
+    }
+}
